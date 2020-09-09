3649250db712e39fda6894a3ff6ded6f6884da8c
Atul Kumar Singh <atul.kumar@codelogicx.com>
2020-09-09 20:43:04 +0530

    poll app with store animation and all

---
+++ src/App.svelte
@@ -0,0 +1,58 @@
+<script>
+import Header from './components/Header.svelte';
+import Footer from './components/Footer.svelte';
+import CreatePollForm from './components/CreatePollForm.svelte';
+import PollList from './components/PollList.svelte';
+import Tabs from './shared/Tabs.svelte';
+//tabs
+let items = ['Current Polls', 'Add new polls'];
+let activeItem = 'Current Polls';
+const tabchange = (e) => {
+  activeItem = e.detail;
+}
+
+const handleAddPoll = (e) => {
+  // const poll = e.detail
+  // polls = [poll,...polls]
+  // console.log(polls);
+  activeItem = 'Current Polls'
+
+}
+
+// const handleVoteUpdate = (e) => {
+//       const {id,option} = e.detail
+//       let copiedPolls = [...polls]
+//       let upvotedPoll = copiedPolls.find((poll) => poll.id == id);
+//       if(option === 'a'){
+//         upvotedPoll.votesA++;
+//       }
+//       if(option === 'b'){
+//         upvotedPoll.votesB++;
+//       }
+
+//       polls = copiedPolls;
+      
+// }
+
+
+
+
+</script>
+<Header />
+<main>
+   <Tabs {activeItem} {items} on:tabchange={tabchange}/>
+   {#if activeItem === 'Current Polls'}
+  <PollList />
+   {:else if activeItem ==='Add new polls'}
+   <CreatePollForm on:add_poll={handleAddPoll}/>
+   {/if}
+</main>
+<Footer />
+
+<style>
+	main{
+    width: 100%;
+    max-width: 960px;
+    margin: 40px auto;
+  }
+</style>
---
+++ src/components/PollList.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { fade, slide, scale } from 'svelte/transition';
+  import { flip } from 'svelte/animate';
+// import {onMount,onDestroy } from 'svelte'
+import PollStore from '../stores/PollStore.js'
+import PollDetails from './PollDetails.svelte';
+
+
+// const unsub =   PollStore.subscribe(data => {
+//      polls = data; 
+//   }); 
+// onDestroy (() => {
+//  unsub();
+// })
+
+
+
+</script>
+
+<div class="poll-list">
+   {#each $PollStore as poll (poll.id)}
+    <div in:fade out:scale|local animate:flip={{duration: 500}}>
+      <PollDetails {poll} />
+    </div>
+  {/each}
+</div>
+
+<style>
+  .poll-list{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+  }
+</style>
